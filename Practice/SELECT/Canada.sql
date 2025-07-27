6ba0892fca21b421e05353e26f7aa46814c1d1cf
Eka Dwipayana <86109298+imdwipayana@users.noreply.github.com>
2025-07-27 12:23:38 +0800

    Update Canada.sql

--- Practice/SELECT/Canada.sql
+++ Practice/SELECT/Canada.sql
@@ -1,87 +1,86 @@
 --=================================================================================
--- DROP player table if it exists
+-- DROP Canada_data table if it exists
 --=================================================================================
-DROP TABLE IF EXISTS book_library;
+DROP TABLE IF EXISTS Canada_data;
 
 --=================================================================================
--- CREATE player table
+-- CREATE Canada_data table year 2021
 --=================================================================================
-CREATE TABLE book_library(
-book_id VARCHAR(15) PRIMARY KEY,
-book_title VARCHAR(200),
-borrowed_id VARCHAR(50),
-borrowed_date DATE,
-return_date DATE
+CREATE TABLE Canada_data(
+province_teritory VARCHAR(50) PRIMARY KEY,
+capital VARCHAR(50),
+population INTEGER,
+growth_rate FLOAT,
+areas FLOAT,
+house INTEGER,
+senate INTEGER,
+border_with_USA VARCHAR(10)
 );
 
 --=================================================================================
 -- INSERT player table value
 --=================================================================================
-INSERT INTO book_library
+INSERT INTO Canada_data
 VALUES 
-('A1001', 'To Kill a Mockingbird',  'W2206', CURRENT_DATE - INTERVAL '55 days', CURRENT_DATE - INTERVAL '25 days'),
-('A1002', 'The Alchemist',          'M0609', CURRENT_DATE - INTERVAL '50 days', NULL),
-('B2001', 'No Country for Old Man', 'T2305', CURRENT_DATE - INTERVAL '45 days', CURRENT_DATE - INTERVAL '20 days'),
-('B2002', 'Cloud Cuckoo Land',      'F1512', CURRENT_DATE - INTERVAL '35 days', NULL),
-('C3001', 'The Grapes of Wrath',    'S0511', CURRENT_DATE - INTERVAL '31 days', CURRENT_DATE - INTERVAL '15 days'),
-('C3002', 'Harry Potter',           'S0511', CURRENT_DATE - INTERVAL '25 days', NULL),
-('C3003', 'River Sing Me Home',     'M1809', CURRENT_DATE - INTERVAL '15 days', NULL);
+('Yukon',                     'Whitehorse',     40232,    12.1, 474712.68,  1,   1,  'No'),
+('British Columbia',          'Victoria',       5000879,  7.6,  922503.01,  43,  6,  'Yes'),
+('Northwestern Teritories',   'Yellowknife',    41070,    -1.7, 1143793.86, 1,   1,  'No'),
+('Alberta',                   'Edmonton',       4262635,  4.8,  640330.46,  37,  6,  'Yes'),
+('Saskatchewan',              'Regina',         1132505,  3.1,  588243.54,  14,  6,  'Yes'),
+('Nunavut',                   'Iqaluit',        36858,    2.5,  1877778.53, 1,   1,  'No'),
+('Manitoba',                  'Winipeg',        1342153,  5,    552370.99,  14,  6,  'Yes'),
+('Ontario',                   'Toronto',        14223942, 5.8,  908699.33,  122, 24, 'Yes'),
+('Quebec',                    'Quebec City',    8501833,  4.1,  1356625.27, 78,  24, 'Yes'),
+('New Brunswick',             'Fredericton',    775610,   3.8,  71388.81,   10,  10, 'Yes'),
+('Newfoundland and Labrador', 'St. Johns',      510550,   -1.8, 370514.08,  7,   6,  'No'),
+('Nova Scotia',               'Halifax',        969383,   5,    52942.27,   11,  10, 'No'),
+('Prince Edward Island',      'Charlotte Town', 154331,   8,    5686.03,    4,   4,  'No');
 
 --=================================================================================
--- Call the book_library table
+-- Call the Canada_data table
 --=================================================================================
-SELECT * FROM book_library
+SELECT * FROM Canada_data;
 
 --=================================================================================
--- Save table which books have been returned
+-- Count population density in column population_density (people/area)
 --=================================================================================
-CREATE TABLE book_returned AS
-SELECT * FROM book_library
-WHERE return_date IS NOT NULL;
+SELECT
+	province_teritory,
+	population,
+	areas,
+	population / areas as population_density
+FROM Canada_data
 
 --=================================================================================
--- Call the book_returned table
+-- House and Senante representation
 --=================================================================================
-SELECT * FROM book_returned;
+SELECT
+	province_teritory,
+	population,
+	house,
+	population/house as house_rep,
+	senate,
+	population/senate as senate_rep
+FROM Canada_data
 
 --=================================================================================
--- Save table which books haven't been returned yet
+-- Population in 2020
+-- NOTE that population2021 = population2020+population2020*growth_rate/100
+-- So that population2020 = population2021/(1+(growth_rate/100))
 --=================================================================================
-CREATE TABLE book_not_returned AS
-SELECT * FROM book_library
-WHERE return_date IS NULL;
+SELECT
+	province_teritory,
+	population,
+	growth_rate,
+	population/(1+(growth_rate/100)) as population_2020
+FROM Canada_data
 
 --=================================================================================
--- Call the book_not_returned table
+-- Population province and teritory that have border with The USA
 --=================================================================================
-SELECT * FROM book_not_returned;
+SELECT
+	border_with_USA,
+	SUM(population) as population_in_border
+FROM Canada_data
+GROUP BY border_with_USA
 
---=================================================================================
--- Save table which books that late for more than 30 days
---=================================================================================
-CREATE TABLE book_late AS
-SELECT *,
-	CURRENT_DATE - borrowed_date as days_borrowed
-FROM book_library
-WHERE CURRENT_DATE - borrowed_date > 30;
-
---=================================================================================
--- Call the book_not_returned table
---=================================================================================
-SELECT * FROM book_late;
-
---=================================================================================
--- Save table with title and number of days borrowed for books 
--- that haven't been returned
---=================================================================================
-CREATE TABLE days_borrowed AS
-SELECT 
-	book_title,
-	CURRENT_DATE - borrowed_date as days_borrowed
-FROM book_library
-WHERE return_date is NULL;
-
---=================================================================================
--- Call the book_not_returned table
---=================================================================================
-SELECT * FROM days_borrowed;
